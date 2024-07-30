a1f64c1b6b0ab537ec9023d0979c16e92ee82b13
maverik-io <josephchacko2006@gmail.com>
2024-07-30 22:50:59 +0530

    added 'says' functionality

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
-import pygame as pg
-from sys import exit
+#!/usr/local/bin/python3
+from sys import argv
 
-pg.init()
-screen = pg.display.set_mode((400, 400))
 
-while True:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            exit()
+def split_linewise(jon_code):
+    lines = []
+    for line in jon_code.split("."):
+        lines.append(line.strip())
 
-    screen.fill('white')
+    return lines
 
-    pg.display.flip()
 
+def parse(line):
+    tokens = []
+    token = ""
+    inside_quotes = ""
+    for c in line:
+        if c == '"':
+            inside_quotes = not inside_quotes
+        elif c == " " and not inside_quotes:
+            if token != "":
+                tokens.append(token)
+            token = ""
+
+        else:
+            token += c
+    tokens.append(token)
+
+    if tokens[0] != "jon":
+        raise Exception("statement does not begin with \"jon\"")
+
+    match tokens[1]:
+        case "says":
+            return f"print('{tokens[2]}')"
+
+
+def main():
+    file = argv[0].split("/")[0] + "/" + argv[1]
+    try:
+        with open(file, 'r') as f:
+            jon_code = f.read()
+    except FileNotFoundError:
+        print(file, "does not exist...")
+        exit()
+
+    lines = split_linewise(jon_code)
+
+    out = []
+
+    for line in lines:
+        out.append(parse(line))
+
+    print(out)
+
+    print('~'*10)
+    for expression in out:
+        exec(expression)
+
+
+if __name__ == "__main__":
+    main()
